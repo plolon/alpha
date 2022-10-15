6d146e9d157e8e11af742384293e8e80422a7395
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-10-15 11:29:01 +0200

    Created dbContext

---
+++ PlatformService/Data/PlatformDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformDbContext : DbContext
+    {
+        public PlatformDbContext(DbContextOptions<PlatformDbContext> options)
+        {
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
---
+++ PlatformService/Extensions/PersistenceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+namespace PlatformService.Extensions
+{
+    public static class PersistenceRegistration
+    {
+        public static IServiceCollection ConfigurePersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<PlatformDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMem");
+                //options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"));
+            });
+
+            return services;
+        }
+    }
+}
