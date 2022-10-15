e7d466750d2c68871fd09e7279c7096711f8b156
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-10-15 22:52:12 +0200

    Added dtos with mapping profile

---
+++ PlatformService/Extensions/ProfilesRegistration.cs
@@ -0,0 +1,14 @@
+﻿using PlatformService.Profiles;
+
+namespace PlatformService.Extensions
+{
+    public static class ProfilesRegistration
+    {
+        public static IServiceCollection ConfigureProfiles(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(PlatformsProfile));
+
+            return services;
+        }
+    }
+}
