425f9be319fbcdc7bb8191d5f496963d86525463
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2022-10-16 09:09:21 +0200

    Added getall endpoint for paltforms

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Dtos;
+using PlatformService.Persistence.IRepositories;
+
+namespace PlatformService.Controllers
+{
+    [ApiController]
+    public class PlatformsController :ControllerBase
+    {
+        private readonly IPlatformRepository _platformRepository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepository platformRepository, IMapper mapper)
+        {
+            _platformRepository = platformRepository;
+            _mapper = mapper;
+        }
+
+        // GET: /api/platforms
+        public async Task<IEnumerable<PlatformReadDto>> Get()
+        {
+            var platforms = await _platformRepository.GetAll();
+            return _mapper.Map<IEnumerable<PlatformReadDto>>(platforms);
+        }
+    }
+}
